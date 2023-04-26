238f390fd86358346e28e445111b8dfc0f5c15ae
NiklasEdvall <niklasedvall@gmail.com>
2023-04-26 16:14:06 +0200

    fixed minor merge conflict

--- analys.R
+++ analys.R
@@ -110,7 +110,6 @@ names(dat)[names(dat) == "F28"] <- "home.lb.type"
 names(dat)[names(dat) == "F29"] <- "home.lb.source"
 
 #Create new variables =========================================================
-
 #sum and recode of laterality
 dat$tin.lat.sum <- dat$F11_1 + dat$F11_2 + dat$F11_3 + dat$F11_4 + dat$F11_5 + dat$F11_6 + dat$F11_7 + dat$F11_8
 dat$tin.lat.recode <- (paste(dat$F11_1, dat$F11_2, dat$F11_3, dat$F11_4, dat$F11_5, dat$F11_6, dat$F11_7, dat$F11_8, sep=""))
@@ -121,15 +120,12 @@ dat$sleep.lb.type.sum <- dat$F22_1 + dat$F22_2 + dat$F22_3 + dat$F22_4 + dat$F22
 #Sum of tinnitus type home
 dat$home.lb.type.sum <- dat$F28_1 + dat$F28_2 + dat$F28_3 + dat$F28_4 + dat$F28_5 + dat$F28_6
 
-<<<<<<< HEAD
 #Tinnitus duration in months
 dat$tin.dur.months <- interval(dat$first.tin.date, "2023-04-26") %/% months(1)
-=======
+
 dat$tin.dur <- 2023 - as.integer(substr(dat$first.tin.date,1,4))
 
 #Specify some useful variables and functions
->>>>>>> abd54175d2b7095a017c3207bf6ace33cc24aed7
-
 #Ha use months
 dat$device.dur.months <- interval(dat$ha.date, "2023-04-26") %/% months(1)
 
@@ -144,11 +140,8 @@ BinToDec <- function(x)
 
 #Descriptives =================================================================
 
-<<<<<<< HEAD
 tab1 <- tableby(sex ~ age + hearing + device + tin.con.occ + tin.dur.months + tin.lat + tin.pitch + tin.loud.varies + tin.reduce.env + sound.tolerance + sound.worse.tin, data = dat)
-=======
-tab1 <- tableby(sex ~ age + hearing + device + tin.con.occ + tin.lat + tin.pitch + tin.dur + tin.loud.varies + tin.reduce.env + sound.tolerance + sound.worse.tin, data = dat)
->>>>>>> abd54175d2b7095a017c3207bf6ace33cc24aed7
+
 summary(tab1, text = TRUE, test = FALSE)
 
 arsenal::write2word(tab1, "descriptives_arsenal.docx", title = "My table",
@@ -286,7 +279,6 @@ rownames(t.res) <- c("sleep an", "sleep aw", "sleep lo", "home an", "home aw", "
 t.res <- cbind(rownames(t.res), data.frame(t.res, row.names=NULL))
 
 colnames(t.res) <- c("Group/NRS" , "Yes - Mean", "Yes - SD", "No - Mean", "No - SD", "t", "df", "p", "p corrected (BH)")
-
 
 #write df to xlsx
 write_xlsx(t.res, "t_test_results.xlsx")
